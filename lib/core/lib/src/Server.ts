f320b536b2d8d3f68904178327a2f3641387109a
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-08-31 12:34:07 +0200

    create proper core plugin

---
+++ lib/core/lib/src/Server.ts
@@ -0,0 +1,60 @@
+// deps
+
+    // externals
+    import { Server } from "node-pluginsmanager-plugin";
+
+// types & interfaces
+
+    // locals
+    import type MediatorCore from "./Mediator";
+    import type { components } from "./Descriptor";
+
+// module
+
+export default class ServerCore extends Server {
+
+    public _initWorkSpace (): Promise<void> {
+
+        (this._Mediator as MediatorCore)
+
+            .on("initialized", this._onPluginInitialized)
+            .on("released", this._onPluginReleased)
+            .on("error", this._onPluginError);
+
+        return Promise.resolve();
+
+    }
+
+    public _releaseWorkSpace (): Promise<void> {
+
+        (this._Mediator as MediatorCore)
+
+            .off("initialized", this._onPluginInitialized)
+            .off("released", this._onPluginReleased)
+            .off("error", this._onPluginError);
+
+        return Promise.resolve();
+
+    }
+
+    // <events>
+
+    private readonly _onPluginInitialized = (): void => {
+
+        this.push("initialized");
+
+    };
+
+    private readonly _onPluginReleased = (): void => {
+
+        this.push("released");
+
+    };
+
+    private readonly _onPluginError = (data: components["schemas"]["PushEventPluginError"]["data"]): void => {
+
+        this.push("error", data);
+
+    };
+
+}
