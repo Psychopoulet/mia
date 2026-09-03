08bdcdea50409b360864deb07d1ab37b82c5cdb7
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-09-03 18:38:15 +0200

    force plugin load order

--- lib/src/tools/init/managePlugins.ts
+++ lib/src/tools/init/managePlugins.ts
@@ -96,7 +96,9 @@ export default function managePlugins (container: ContainerPattern): Promise<voi
             logger.warning("Plugin " + pluginName + " uninstalled");
         });
 
-    return pluginsManager.addExternalPluginDirectory(join(__dirname, "..", "..", "..", "..", "plugins", "mia-core")).then((): Promise<void> => {
+    return pluginsManager.setOrder([ "mia-core" ]).then((): Promise<void> => {
+        return pluginsManager.addExternalPluginDirectory(join(__dirname, "..", "..", "..", "..", "plugins", "mia-core"));
+    }).then((): Promise<void> => {
         return pluginsManager.loadAll(container);
     }).then((): Promise<void> => {
         return pluginsManager.initAll(container);
